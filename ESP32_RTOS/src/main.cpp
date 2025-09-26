193349cd6f04771b5b576dd4aebe3a8423541cef
aerasMAC <aeraslan80@gmail.com>
2025-09-26 16:08:56 +0300

    Tüm herşey yazıldı geliştirme kartına yüklendi

--- ESP32_RTOS/src/main.cpp
+++ ESP32_RTOS/src/main.cpp
@@ -11,6 +11,52 @@ volatile bool gorevSuspended = false;
 volatile uint32_t sonInterruptZamani = 0;
 const uint32_t debounceSuresi = 100; 
 
+
+
+void IRAM_ATTR buttonISR() ;
+void BlinkGorev(void *parameter) 
+{
+  for (;;) 
+  { 
+    digitalWrite(LED_Pin, HIGH);
+    Serial.println("BlinkGorev: LED ON");
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000ms
+    digitalWrite(LED_Pin, LOW);
+    Serial.println("BlinkGorev: LED OFF");
+    Serial.print("BlinkGorev cekirdek uzerinde calisiyor: ");
+    Serial.println(xPortGetCoreID());
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(LED_Pin, OUTPUT);
+  pinMode(Buton_Pin, INPUT_PULLUP); // Internal pull-up resistor
+
+  // Kesme butonu eklendi
+  attachInterrupt(digitalPinToInterrupt(Buton_Pin), buttonISR, FALLING);
+
+  // Gorev oluşturma
+  xTaskCreatePinnedToCore(
+    BlinkGorev,         // Gorev fonksiyonu
+    "BlinkGorev",       // Gorev adı
+    10000,             // Yığın Boyutu (bytes)
+    NULL,              // parametreler
+    1,                 // Öncelik
+    &BlinkGorevHandle,  // Gorev handle
+    1                  // Çekirdek 1
+  );
+}
+
+void loop() 
+{
+  
+}
+
 void IRAM_ATTR buttonISR() 
 {
   // Debounce kontrolü
@@ -33,45 +79,4 @@ void IRAM_ATTR buttonISR()
     vTaskResume(BlinkGorevHandle);
     Serial.println("BlinkTask devam ediyor");
   }
-}
-
-
-void BlinkGorev(void *parameter) 
-{
-  for (;;) 
-  { 
-    digitalWrite(LED_Pin, HIGH);
-    Serial.println("BlinkGorev: LED ON");
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1000ms
-    digitalWrite(LED_Pin, LOW);
-    Serial.println("BlinkGorev: LED OFF");
-    Serial.print("BlinkGorev cekirdek uzerinde calisiyor: ");
-    Serial.println(xPortGetCoreID());
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
-void setup() 
-{
-  Serial.begin(115200);
-  pinMode(LED_Pin, OUTPUT);
-  pinMode(Buton_Pin, INPUT_PULLUP); // Internal pull-up resistor
-
-  // Kesme butonu eklendi
-  attachInterrupt(digitalPinToInterrupt(Buton_Pin), buttonISR, FALLING);
-
-  // Gorev oluşturma
-  xTaskCreatePinnedToCore(
-    BlinkGorev,         // Gorev fonksiyonu
-    "BlinkGorev",       // Gorev adı
-    10000,             // Yığın Boyutu (bytes)
-    NULL,              // parametreler
-    1,                 // Öncelik
-    &BlinkGorevHandle,  // Gorev handle
-    1                  // Çekirdek 1
-  );
-}
-
-void loop() 
-{
-  
 }
