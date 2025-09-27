306477f87c988f1a065a94fac4d6c7f75733c183
aerasMAC <aeraslan80@gmail.com>
2025-09-27 12:33:01 +0300

    pin ve pwm tanımlamaları yapıldı

--- ESP32_FreeRTOS_Queues/src/main.cpp
+++ ESP32_FreeRTOS_Queues/src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
-#include <esp32-hal-ledc.h>
-#define POT_PIN 15 
-#define LED_PIN 2   
-#define PWM_FREQ 5000
-#define PWM_RESOLUTION 12  // 12-bit (0–4095)
-#define QUEUE_SIZE 5
 
-// Create an handle for the queue
+#define POT_Pin 15 
+#define LED_Pin 2   
+#define PWM_FREK 5000
+#define PWM_COZUNURLUK 12  // 12-bit (0–4095)
+#define QUEUE_BOYUT 5
+
+// Kuyruk için handle oluşturuldu
 QueueHandle_t potQueue = NULL;
 
 void potTask(void *parameter) {
   for (;;) {
-    uint16_t potValue = analogRead(POT_PIN);  // Read 0–4095
+    uint16_t potValue = analogRead(POT_Pin);  // Read 0–4095
     xQueueSend(potQueue, &potValue, portMAX_DELAY);  // Send to queue
     Serial.printf("potTask: Sent pot value %u\n", potValue);
     vTaskDelay(100 / portTICK_PERIOD_MS);  // 100ms
@@ -23,7 +23,7 @@ void LEDBrightnessTask(void *parameter) {
     uint16_t potValue;
     if (xQueueReceive(potQueue, &potValue, portMAX_DELAY)) {
       uint16_t brightness = potValue;
-      ledcWrite(LED_PIN, brightness);
+      ledcWrite(LED_Pin, brightness);
       Serial.printf("LEDBrightnessTask: Set brightness %u\n", brightness);
     }
   }
@@ -33,10 +33,10 @@ void setup() {
   Serial.begin(115200);
 
   // Setup PWM for LED
-  ledcAttach(LED_PIN, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttach(LED_Pin, PWM_FREK, PWM_COZUNURLUK);
 
   // Create queue (5 items, each uint16_t)
-  potQueue = xQueueCreate(QUEUE_SIZE, sizeof(uint16_t));
+  potQueue = xQueueCreate(QUEUE_BOYUT, sizeof(uint16_t));
   if (potQueue == NULL) {
     Serial.println("Failed to create queue!");
     while (1);
